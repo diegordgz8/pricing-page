ac45bda1daf54242b487ddd3893f6dc0232a2ddf
Diego A. Rodríguez <diegordgz8@outlook.com>
2023-07-11 14:12:37 -0400

    update: add faqs section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    container: { center: true },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     extend: {
       colors: {
         theme: {
-          400: "#00b289",
+          50: "#e6f7f3",
+          100: "#ccf0e7",
+          200: "#99e0d0",
+          300: "#66d1b8",
+          400: "#33c1a1",
+          500: "#00b289",
+          600: "#008e6e",
+          700: "#006b52",
+          800: "#004737",
+          900: "#00241b",
         },
       },
     },
